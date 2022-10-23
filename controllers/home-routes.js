1c0d04284a9fd22a0848168aaf5f426b50fa64ec
Avialmonanay <108016215+Avialmonanay@users.noreply.github.com>
2022-10-23 15:32:57 -0600

    post routes generated and rough draft finished. File structure updated to include all routes. user routes added.

---
+++ controllers/home-routes.js
@@ -0,0 +1,68 @@
+const router = require('express').Router();
+const {Products} = require('../models');
+// Import the custom middleware
+const withAuth = require('../utils/auth');
+
+
+// render the homepage
+router.get('/', async (req, res) => {
+  try {
+    res.render('homepage', {
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// GET one product and render a page for it.
+router.get('/product/:id', async (req, res) => {
+  try {
+    const productData = await Products.findByPk(req.params.id);
+
+    const product = productData.get({ plain: true });
+    res.render('product', { product, loggedIn: req.session.loggedIn });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+//GET all products with a matching zip code and render them on page.
+router.get('/search/:zip', async (req, res) => {
+  try {
+    const searchData = await Products.findAll({where:{zip_code:req.params.zip}});
+
+    const search = searchData.get({ plain: true });
+
+    res.render('search', { search, loggedIn: req.session.loggedIn });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+//render the login page or redirect to homepage if the user is already logged in.
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
+//if the user is logged in render the post page. If not then redirect them to the log in page.
+router.get('/post', withAuth, (req, res) => {
+    if (req.session.loggedIn) {
+    res.render('post');
+      return;
+    }
+    res.redirect('/login');
+    
+  });
+
+
+
+module.exports = router;
