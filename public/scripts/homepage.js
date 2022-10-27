e3665cfff7f490f8fe75b72f0415abb1b0b8c523
Noah Brimhall <noahbrimhall6@gmail.com>
2022-10-26 18:41:16 -0600

    Created homepage.js

---
+++ public/scripts/homepage.js
@@ -0,0 +1,11 @@
+$(".mainSearch").click(function (event) {
+  event.preventDefault();
+  var element = event.target;
+  if (element.matches("button")) {
+    var userInput = $(this).children("input").val();
+    console.log(userInput);
+    location.replace(`search/${userInput}`);    
+  }
+});
+
+$(".card").click(event => location.replace(`product/${$(this).data(id)}`));
--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,11 +1,15 @@
 $(".search").click(function (event) {
-    event.preventDefault()
-    var element = event.target
+    event.preventDefault();
+    var element = event.target;
     if (element.matches("button")) {
-        var userInput = $(this).children("input").val()
-        console.log("Hello")
-        location.replace(`search/${userInput}`)
-        
+        var userInput = $(this).children("input").val();
+        console.log(userInput);
+        location.replace(`search/${userInput}`);    
     }
-})
+});
 
+$(".home").click(event => location.replace("/"));
+
+$(".login").click(event => location.replace("/login"));
+
+$(".post").click(event => location.replace("/post"));
